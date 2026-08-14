fb737a28503009e5007b54891c16a57ccfa11f2c
nickiidev <nickiinrs@gmail.com>
2026-08-14 20:32:15 -0300

    Delete Sistema de biblioteca (1)

--- Sistema de biblioteca (1)/Sistema de biblioteca/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-// A interface diz O QUE o repositório deve fazer, mas não COMO.
-public interface LivroRepository {
-    void salvar(Livro livro);
-    Livro buscarPorId(int id);
-    void atualizarDisponibilidade(int id, boolean disponivel);
-    List<Livro> listarTodos();
-}
--- Sistema de biblioteca (1)/Sistema de biblioteca/UsuarioRepository.java
+++
@@ -1,5 +0,0 @@
-public interface UsuarioRepository {
-    void salvar(Usuario usuario);
-    Usuario buscarPorId(int id);
-    void atualizarQuantidadeEmprestimos(int id, int novaQuantidade);
-}
